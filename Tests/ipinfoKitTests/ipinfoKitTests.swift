38d58ac200e48c976d58512c856414a818fc5dc7
muhammadahmadmslm <muhammad.ahmad@mslm.io>
2023-04-11 12:56:23 +0500

    Initial Commit

---
+++ Tests/ipinfoKitTests/ipinfoKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ipinfoKit
+
+final class ipinfoKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ipinfoKit().text, "Hello, World!")
+    }
+}
