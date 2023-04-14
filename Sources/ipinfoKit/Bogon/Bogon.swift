532d10ee4dddb9d7f5ccb878d68146991264271b
muhammadahmadmslm <muhammad.ahmad@mslm.io>
2023-04-14 13:46:07 +0500

    --update

--- Sources/ipinfoKit/Bogon/Bogon.swift
+++ Sources/ipinfoKit/Bogon/Bogon.swift
@@ -25,11 +25,11 @@ extension IPINFO{
             "224.0.0.0/4", // Multicast (RFC 5771)
             "240.0.0.0/4" // Reserved (RFC 1112)
         ]
-
+        
         for range in bogonRanges {
             let cidrComponents = range.split(separator: "/")
             let ipRange = cidrComponents[0]
-            let subnetMask = UInt32(cidrComponents[1])!
+            let subnetMask = UInt32(cidrComponents[1]) ?? UInt32()
             let ipAddressInt = ipToUInt32(String(ipString))
             let ipRangeInt = ipToUInt32(String(ipRange))
             let subnetMaskInt = (0xFFFFFFFF as UInt32) << (32 - subnetMask)
@@ -40,7 +40,7 @@ extension IPINFO{
         return false
     }
     func ipToUInt32(_ ipAddress: String) -> UInt32 {
-        let parts = ipAddress.split(separator: ".").map { UInt8($0)! }
+        let parts = ipAddress.split(separator: ".").map { UInt8($0) ?? UInt8() }
         return (UInt32(parts[0]) << 24) | (UInt32(parts[1]) << 16) | (UInt32(parts[2]) << 8) | UInt32(parts[3])
     }
 }
--- Sources/ipinfoKit/Cache/Cache.swift
+++ Sources/ipinfoKit/Cache/Cache.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+
 extension UserDefaults {
     func set<T: Encodable>(encodable: T, forKey key: String) {
         if let data = try? JSONEncoder().encode(encodable) {
