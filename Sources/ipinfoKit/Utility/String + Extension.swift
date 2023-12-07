a8fe030fb1dfa1c15cd8c1df8363a243e3c71061
Muhammad Ahmad <118795373+muhammadahmadmslm@users.noreply.github.com>
2023-12-08 03:16:59 +0500

    -- update

--- Sources/ipinfoKit/Utility/String + Extension.swift
+++ Sources/ipinfoKit/Utility/String + Extension.swift
@@ -14,9 +14,9 @@ extension String {
 		let ipv6Pattern =
 			#"^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9])?[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9])?[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9])?[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9])?[0-9]))$"#
         
-		if let IPv4 = range(of: ipv4Pattern, options: .regularExpression) {
+		if let _ = range(of: ipv4Pattern, options: .regularExpression) {
 			return .IPv4
-		} else if let IPv6 = range(of: ipv6Pattern, options: .regularExpression) {
+		} else if let _ = range(of: ipv6Pattern, options: .regularExpression) {
 			return .IPv6
 		} else {
 			return .none
